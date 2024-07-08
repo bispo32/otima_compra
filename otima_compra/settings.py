e998e8cd932155c9f60e1e4fcde91150c3d388e0
talibispo <mundonovo369@gmail.com>
2024-07-08 13:14:08 -0300

    Prepare for deployment

--- otima_compra/settings.py
+++ otima_compra/settings.py
@@ -113,7 +113,15 @@ USE_I18N = True
 
 USE_TZ = True
 
+# Static files (CSS, JavaScript, Images)
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+# Media files
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+# Outras configurações do Django
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
